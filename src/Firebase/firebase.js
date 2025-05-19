900d1631b7df78803097d6a16247dc418fc0118f
Athul-Sagar <athulsagar@gmail.com>
2025-05-20 02:13:22 +0530

    added .env

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,23 +1,18 @@
 // Firebase/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; // Only need getAuth here
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvoaxX8zdtVTsFmP6KU5jeg46THH1OrB0",
-  authDomain: "ferrous-osprey-453310-c4.firebaseapp.com",
-  projectId: "ferrous-osprey-453310-c4",
-  storageBucket: "ferrous-osprey-453310-c4.firebasestorage.app",
-  messagingSenderId: "584311092618",
-  appId: "1:584311092618:web:3f9925348dbba27cd88473",
-  measurementId: "G-BR17BQL3JR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
 const auth = getAuth(app);
 
-// Export only the initialized instances (app and auth)
 export { app, auth };
